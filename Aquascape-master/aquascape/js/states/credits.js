a137acb546e13e28d95be73c0d47ceea4fd45a05
jarg3n <38875816+jarg3n@users.noreply.github.com>
2018-05-15 15:04:46 -0700

    Add files via upload
    Meeting changes

---
+++ Aquascape-master/aquascape/js/states/credits.js
@@ -0,0 +1,15 @@
+// Aquascape Credits state.
+
+var Credits = function(game) {};
+Credits.prototype = {
+	preload: function() {
+		console.log('Credits; preload');
+	},
+
+	create: function() {
+		console.log('Credits: create');
+		game.state.start('Load');
+	},
+
+	update: function() {}
+}
